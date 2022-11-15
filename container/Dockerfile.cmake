888bedfe8ec0137358df376111f2ed8f4e07e9e6
mathdugre <mathdugre@pm.me>
2022-11-15 13:21:22 -0500

    [ADD] ANTs with debug info.

---
+++ container/Dockerfile.cmake
@@ -0,0 +1,33 @@
+FROM ubuntu:focal-20221019
+
+ENV DEBIAN_FRONTEND="noninteractive" \
+    LANG="en_US.UTF-8" \
+    LC_ALL="en_US.UTF-8"
+RUN : \
+    && apt-get update \
+    && apt-get install -y --no-install-recommends \
+      apt-utils \
+      apt-transport-https \
+      build-essential \
+      ca-certificates \
+      gnupg \
+      wget \
+    && :
+
+# Add GPG key for CMAKE
+RUN wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null \
+      | gpg --dearmor - \
+      | tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null \
+    && echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ focal main' \
+      | tee /etc/apt/sources.list.d/kitware.list >/dev/null 
+
+# Install CMAKE
+ARG CMAKE_VERSION="3.24.1-0kitware1ubuntu20.04.1"
+RUN : \
+    && apt-get update \
+    && apt-get install -y --no-install-recommends \
+      cmake=${CMAKE_VERSION} \
+      cmake-data=${CMAKE_VERSION} \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
+    && :
