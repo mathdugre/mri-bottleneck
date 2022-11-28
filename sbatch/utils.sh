0885605117a35ae87ee772c4f39040c65d4cbb19
mathdugre <mathdugre@pm.me>
2022-11-28 17:54:08 -0500

    [REMOVE] profiling dir and nthread slurm overidde.
    It was creating unessecary issues.

--- sbatch/utils.sh
+++ sbatch/utils.sh
@@ -17,8 +17,6 @@ Usage: sh ${FUNC} <SIF_IMG> -i <INPUT_DIR> -o <OUTPUT_DIR> [-j <NTHREAD>]
     -j NTHREAD: Number of threads used for the application. By default use all.
 
     -o OUTPUT_DIR: Path to the output directory.
-
-    -p PROFILING_DIR: Path to write profiling data.
 "
 
 # Initalize env loading on either Slashbin and Compute Canada
@@ -58,10 +56,6 @@ while (( $# )); do
         validate_opt $@
         OUTPUT_DIR=$2
         shift 2;;
-    -p | --profiling)
-        validate_opt $@
-        PROFILING_DIR=$2
-        shift 2;;
     -* | --*=) # unsupported flags
       echo "Error: Unsupported flag ${1}" >&2
       exit 1
@@ -88,16 +82,10 @@ if [[ ! -f ${SIF_IMG} ]]; then
     echo "Error: SIF_IMG path does not exist: ${SIF_IMG} "
     exit 1
 fi
-if [[ -n ${SLURM_NPROCS}]]; then
-    NTHREAD=$((NTHREAD>SLURM_NPROCS ? SLURM_NPROCS : NTHREAD))
-    echo "WARNING: ${SLURM_NPROCS} core(s) allocated by SLURM. NTHREAD will be overidden to ${SLURM_NPROCS}"
-fi
-mkdir -p ${PROFILING_DIR}
 
 export SIF_IMG
 export INPUT_DIR
 export OUTPUT_DIR
-export PROFILING_DIR
 export NTHREAD
 export DATASET=$(basename ${INPUT_DIR})
 export SUBJECT_ID=$(sed -n $(( 1 + ${SLURM_ARRAY_TASK_ID} ))p ${INPUT_DIR}/participants.tsv | cut -f1)
@@ -109,7 +97,6 @@ Pipeline: ${FUNC}
 SIF_IMG: ${SIF_IMG}
 INPUT_DIR: ${INPUT_DIR}
 OUTPUT_DIR: ${OUTPUT_DIR}
-PROFILING_DIR: ${PROFILING_DIR}
 NTHREAD: ${NTHREAD}
 DATASET: ${DATASET}
 SUBJECT_ID: ${SUBJECT_ID}
