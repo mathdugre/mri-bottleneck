183529ef611def0e345407d2f4cdc843c58562f2
mathdugre <mathdugre@pm.me>
2022-11-28 17:25:40 -0500

    [ADD] nthread override for slurm + [ADD] output dir for profiling data.

--- sbatch/utils.sh
+++ sbatch/utils.sh
@@ -17,6 +17,8 @@ Usage: sh ${FUNC} <SIF_IMG> -i <INPUT_DIR> -o <OUTPUT_DIR> [-j <NTHREAD>]
     -j NTHREAD: Number of threads used for the application. By default use all.
 
     -o OUTPUT_DIR: Path to the output directory.
+
+    -p PROFILING_DIR: Path to write profiling data.
 "
 
 # Initalize env loading on either Slashbin and Compute Canada
@@ -26,7 +28,6 @@ then
     mdoule load python/3.10
 fi
 [[ -z ${SLURM_TMPDIR:+x} ]] && export SLURM_TMPDIR=/disk5/${USER}/mri-bottleneck
-NTHREAD=$(nproc)
 
 # Parse arguments
 function validate_opt(){
@@ -36,6 +37,9 @@ function validate_opt(){
         exit 1
     fi
 }
+
+NTHREAD=$(nproc)
+PROFILING_DIR="vtune_output"
 
 PARAMS=""
 while (( $# )); do
@@ -53,6 +57,10 @@ while (( $# )); do
     -o | --output)
         validate_opt $@
         OUTPUT_DIR=$2
+        shift 2;;
+    -p | --profiling)
+        validate_opt $@
+        PROFILING_DIR=$2
         shift 2;;
     -* | --*=) # unsupported flags
       echo "Error: Unsupported flag ${1}" >&2
@@ -80,10 +88,16 @@ if [[ ! -f ${SIF_IMG} ]]; then
     echo "Error: SIF_IMG path does not exist: ${SIF_IMG} "
     exit 1
 fi
+if [[ -n ${SLURM_NPROCS}]]; then
+    NTHREAD=$((NTHREAD>SLURM_NPROCS ? SLURM_NPROCS : NTHREAD))
+    echo "WARNING: ${SLURM_NPROCS} core(s) allocated by SLURM. NTHREAD will be overidden to ${SLURM_NPROCS}"
+fi
+mkdir -p ${PROFILING_DIR}
 
 export SIF_IMG
 export INPUT_DIR
 export OUTPUT_DIR
+export PROFILING_DIR
 export NTHREAD
 export DATASET=$(basename ${INPUT_DIR})
 export SUBJECT_ID=$(sed -n $(( 1 + ${SLURM_ARRAY_TASK_ID} ))p ${INPUT_DIR}/participants.tsv | cut -f1)
@@ -91,12 +105,13 @@ export SUBJECT_ID=$(sed -n $(( 1 + ${SLURM_ARRAY_TASK_ID} ))p ${INPUT_DIR}/parti
 echo "---------------------
 Profiling information
 
-Pipeline: $FUNC
-SIF_IMG: $SIF_IMG
-INPUT_DIR: $INPUT_DIR
-OUTPUT_DIR: $OUTPUT_DIR
-NTHREAD: $NTHREAD
-DATASET: $DATASET
-SUBJECT_ID: $SUBJECT_ID
+Pipeline: ${FUNC}
+SIF_IMG: ${SIF_IMG}
+INPUT_DIR: ${INPUT_DIR}
+OUTPUT_DIR: ${OUTPUT_DIR}
+PROFILING_DIR: ${PROFILING_DIR}
+NTHREAD: ${NTHREAD}
+DATASET: ${DATASET}
+SUBJECT_ID: ${SUBJECT_ID}
 ---------------------
 "
